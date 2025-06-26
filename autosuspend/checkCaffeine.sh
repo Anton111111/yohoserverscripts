42ca8754bc8eebf17b6f21a3bb02f28a3a77d533
Anton Potekhin <anton.potekhin@orbitsoft.com>
2025-06-26 10:36:51 +0300

    .

--- autosuspend/checkCaffeine.sh
+++ autosuspend/checkCaffeine.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
-SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source $SCRIPT_PATH/../.env
-if [ -e "$CAFFEINE_FILE_PATH" ]; 
-then
-	exit 0
+if [ -s "$CAFFEINE_FILE_PATH" ]; then
+	threshold=$(<"$CAFFEINE_FILE_PATH")
+	# Validate threshold is a number
+	if ! [[ "$threshold" =~ ^[0-9]+$ ]]; then
+		echo "Not threshold"
+		exit 1
+	fi
+
+	now=$(date +%s)
+	if [[ "$(uname)" == "Darwin" || "$(uname)" == *"BSD"* ]]; then
+		modTime=$(stat -f %m "$CAFFEINE_FILE_PATH") # macOS or BSD
+	else
+		modTime=$(stat -c %Y "$CAFFEINE_FILE_PATH") # GNU/Linux
+	fi
+
+	# Calculate difference
+	diff=$((now - modTime))
+	if ((diff <= threshold)); then
+		echo "Good"
+		exit 0
+	else
+		echo "Not"
+		echo "0">"$CAFFEINE_FILE_PATH"
+		exit 1
+	fi
 else
 	exit 1
 fi
